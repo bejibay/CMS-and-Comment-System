7bbc4331d5394f768cccd9be51eaeca27cd5b79e
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-02 11:43:54 +0100

    Changes to lines of codes

--- config.php
+++ config.php
@@ -4,14 +4,15 @@
 
 <?php 
 
-define("HOSTNAME", "Localhost");
-define("DATABASE", "soowecca");
-define("USERNAME", "soowecco");
-define("PASSWORD", "");
-
-$conn=mysqli_connect("HOSTNAME", "DATABASE","USERNAME","PASSWORD"); 
-if (!$conn){echo "connection failed". "". mysqli_connect_error();}
-require("functions.php");
+define("DB_DSN", "mysql:host=localhost;dbname=soowecco");
+define("DB_USERNAME", "soowecca");
+define("DB_PASSWORD", " ");
+define("CLASS_PATH", "classes");
+define("TEMPLATE_PATH", "templates"):
+require(classes."/article.php");
+require(classes."/category");
+require(classes."/admin.php");
+require(functions.php);
 
 ?>
 
