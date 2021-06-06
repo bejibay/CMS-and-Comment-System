82ef6ec952388bd575e0c5116adeae85db212084
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-06 22:30:02 +0100

    Update config.php

--- config.php
+++ config.php
@@ -13,6 +13,11 @@ require(classes."/article.php");
 require(classes."/category");
 require(classes."/admin.php");
 require(functions.php);
+function handle_execption($exception){
+echo "there is an error, try again";
+error_log($exeption->getMessage);
+set_exception_handler(handle_exception;
+}
 
 ?>
 
