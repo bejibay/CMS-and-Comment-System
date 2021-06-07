c98c6fa3bb745cfc0a611f8bcb995d074f86161b
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-07 18:01:15 +0100

    changes to lines of codes

--- classes/posts.php
+++ classes/posts.php
@@ -4,14 +4,39 @@
 
 <?php 
 
-define("HOSTNAME", "Localhost");
-define("DATABASE", "soowecca");
-define("USERNAME", "soowecco");
-define("PASSWORD", "");
+ Class Post{
+// define the class properties
+public $id = null;
+public $title = null;
+public $summary =null;
+public $content = null;
+public $url = null; 
+public $imageurl = null;
+public $image_extension = null;
+public $categoryID = null;
+public $timestamp =null;
+public $ip = null;
 
-$conn=mysqli_connect("HOSTNAME", "DATABASE","USERNAME","PASSWORD"); 
-if (!$conn){echo "connection failed". "". mysqli_connect_error();}
-require("functions.php");
+public function __construct($data=array){
+if(isset($data['id']))$this->id=int($data['id']);
+if(isset($data['id']))$this->id=int($data['id']);
+if(isset($data['id']))$this->id=int($data['id']);
+if(isset($data['id']))$this->id=int($data['id']);
+if(isset($data['id']))$this->id=int($data['id']);
+if(isset($data['categoryID']))$this->categoryID=int($data['categoryID']);
+
+
+
+}
+
+
+
+}
+
+
+
+
+
 
 ?>
 
