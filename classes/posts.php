5753a7714e0b4cece54e77a09acbe3936d6e88c9
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-09 08:48:10 +0100

    Update to lines of codes

--- classes/posts.php
+++ classes/posts.php
@@ -14,18 +14,19 @@ public $url = null;
 public $imageurl = null;
 public $image_extension = null;
 public $categoryID = null;
-public $timestamp =null;
+public $pubdate =null;
 public $ip = null;
 
 public function __construct($data=array){
 if(isset($data['id']))$this->id=int($data['id']);
-if(isset($data['id']))$this->id=int($data['id']);
-if(isset($data['id']))$this->id=int($data['id']);
-if(isset($data['id']))$this->id=int($data['id']);
-if(isset($data['id']))$this->id=int($data['id']);
+if(isset($data['pubdate']))$this-pubdate=int($databases['pubdate']);
+if(isset($data['title']))$this->title=preg_replace("/[^\,\.\"\'\:\;\@\$\()a-Z0-9]/","",$data['id']);
+if(isset($data['summary']))$this->summary=preg_replace("/[^\,\.\"\'\:\;\@\$\()a-Z0-9]/","",$data['id']);
 if(isset($data['categoryID']))$this->categoryID=int($data['categoryID']);
-
-
+if(isset($data['url']))$this->url=$data['url'];
+if(isset($data['imageurl']))$this->imageurl=$data['imageurl'];
+if(isset($data['image_extension']))$this->image_extension=$data['image_extension'];
+if(isset($data['ip']))$this->ip=int($data['ip']);
 
 }
 
