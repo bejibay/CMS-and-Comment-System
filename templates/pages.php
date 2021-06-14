50aed1f00705491a36202f4707cc971f16ca12c3
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-14 13:34:59 +0100

    Update pages.php

--- templates/pages.php
+++ templates/pages.php
@@ -1,4 +1,9 @@
-<div class="row">
+<html>
+<head>
+<meta  name="description" content="<?php echo $results['article']->description;?>">
+<meta  name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php echo $results['article']->title;?></title>
+</head>div class="row">
 <div class="column">
 <div>echo $content;</div>
 </div>
