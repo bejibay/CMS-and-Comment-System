e200a79b63c3d8bd80f2cdbc536ddfaed0cf0f24
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-14 13:33:13 +0100

    Update homepage.php

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,4 +1,10 @@
-<div class="row">
+
+<html>
+<head>
+<meta  name="description" content="<?php echo $results['article']->description;?>">
+<meta  name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php echo $results['article']->title;?></title>
+</head> class="row">
 <div class="column leftside">
 <div>echo $content;</div>
 </div>
