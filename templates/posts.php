5f0a82279295f2a9fd4f88d256fb941909eefff3
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-15 05:14:43 +0100

    Update posts.php

--- templates/posts.php
+++ templates/posts.php
@@ -2,16 +2,13 @@
 <head>
 <meta  name="description" content="<?php echo $results['article']->description;?>">
 <meta  name="viewport" content="width=device-width, initial-scale=1.0">
-<title><
-?php echo $results['article']->title;?></title>
+<title><?php echo $results['article']->title;?></title>
 </head>
 <body>
-<div class="header"><?php include "templates/header.php";?><div>
-<?php ®
- <div class="row">
+<div class="header"><?php include "templates/header.php";?></div>
+<div class="row">
 <div class="column leftside">
-<div>echo $content;</div>
+ <?php echo $result['article']->content;?></div>
+</div class="column rightside">put this column content here</div>
 </div>
-</div class="column rightside">$content;</div>
-</div>
-××××
+
