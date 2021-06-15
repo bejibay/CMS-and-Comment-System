935d682c8c3f037fcf25d12f964524a71d6b349c
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-15 05:21:02 +0100

    Update notfound.php

--- templates/notfound.php
+++ templates/notfound.php
@@ -1,9 +1,15 @@
 <html>
 <head>
-<meta  name="description" content="<?php echo $results['article']->description;?>">
+<meta  name="description" content="<?php echo $results['description'];?>">
 <meta  name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo $results['article']->title;?></title>
-</head>div class="row">
-<div class="column">
-<div><h>PAGE NOT FOUND</h></div>
+<title><?php echo $results['title'];?></title>
+</head>
+<body>
+<div class="header"><?php include "templates/header.php";?></div>
+<div class="row">
+<div class="column leftside">
+ This page does not exist</div>
+</div class="column rightside">put this column content here</div>
 </div>
+
+
