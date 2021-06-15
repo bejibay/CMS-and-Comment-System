b5b30c2584047fc24ef30ffa9f6e9591eb1182e2
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-15 05:16:59 +0100

    Update pages.php

--- templates/pages.php
+++ templates/pages.php
@@ -3,7 +3,13 @@
 <meta  name="description" content="<?php echo $results['article']->description;?>">
 <meta  name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php echo $results['article']->title;?></title>
-</head>div class="row">
-<div class="column">
-<div>echo $content;</div>
+</head>
+<body>
+<div class="header"><?php include "templates/header.php";?></div>
+<div class="row">
+<div class="column leftside">
+ <?php echo $result['article']->content;?></div>
+</div class="column rightside">put this column content here</div>
 </div>
+
+
