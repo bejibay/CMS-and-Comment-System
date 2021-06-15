c0601326f7395e8a90153dd4131a89600e319554
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-15 16:58:54 +0100

    Update header.php

--- templates/include/header.php
+++ templates/include/header.php
@@ -1,9 +1,9 @@
 ﻿<div class="topnav">
-<a href="/homepage/">home</a>
-<a href="/about/">About Us</a>
-<a href="/contact/">Contact Us</a>
-<a href="/privacy/">Privacy</a>
-<a href="/register/">Register</a>
-<a href="/login/">login</a>
+<a href="/homepage">home</a>
+<a href="/about">About Us</a>
+<a href="/contact">Contact Us</a>
+<a href="/privacy">Privacy</a>
+<a href="/register">Register</a>
+<a href="/login">login</a>
 <a href="javascript:void()"><i  class ="fa fa-bars" ></a>
 </div>
