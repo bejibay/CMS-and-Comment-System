561a6a21678a7300d5ce555ec906e182790b05f0
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-15 17:13:40 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -9,37 +9,33 @@ $action=isset($_GET['action'])? $_GET['action'] : "";
 $username=isset($username)? $_SESSION['username'] : "";
 ?>
 <!DOCTYPE html>
-
 <html>
 <head>
-<meta  name="description" content="<?php echo $description;?>">
+<meta  name="description" content="<?php echo $result['description'];?>">
 <meta  name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo $title;?></title>
+<title><?php echo $result['title'];?></title>
 </head>
 <body>
 <div class="header"></div>
 <div class="topnav">
-<a href="https://www.soowec.com">home</a>
-<a href="/about/">About Us</a>
-<a href="/contact/">Contact Us</a>
-<a href="/privacy/">Privacy</a>
-<a href="/register/">Register</a>
-<a href="/login/">login</a>
+<a href="/homepage">home</a>
+<a href="/about">About Us</a>
+<a href="/contact">Contact Us</a>
+<a href="/privacy">Privacy</a>
+<a href="/register">Register</a>
+<a href="/login">login</a>
 <a href="javascript:void()"><i  class ="fa fa-bars" ></a>
 </div>
 <div>
 <?php
 switch($action){
 case "login":
-case "login"."/":
 login();
 break;
 
 case "register":
-case "register"."/":
 register();
 break;
-
 case "resetpassword"."/".$reseturl:
 resetpassword();
 break;
