310b72beee37acbb56039581072f93b7eede670e
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-15 05:24:59 +0100

    Update homepage.php

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,12 +1,15 @@
-
 <html>
 <head>
-<meta  name="description" content="<?php echo $results['article']->description;?>">
+<meta  name="description" content="<?php echo $results['description'];?>">
 <meta  name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo $results['article']->title;?></title>
-</head> class="row">
+<title><?php echo $results['title'];?></title>
+</head>
+<body>
+<div class="header"><?php include "templates/header.php";?></div>
+<div class="row">
 <div class="column leftside">
-<div>echo $content;</div>
+ Put the homepage contents here</div>
+</div class="column rightside">put this column content here</div>
 </div>
-</div class="column rightside">$content;</div>
-</div>
+
+
