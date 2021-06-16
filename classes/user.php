1f865be1fff4657ecbc9563d470281fcc20107be
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-16 11:18:30 +0100

    Update user.php

--- classes/user.php
+++ classes/user.php
@@ -4,10 +4,11 @@
 
 <?php 
 
- Class Post{
+ Class User{
 // define the class properties
 public $id = null;
 public $name = null;
+public $email = null;
 public $username =null;
 public $password = null;
 public $reseturl =null;
@@ -19,6 +20,7 @@ if(isset($data['id']))$this->id=int($data['id']);
 if(isset($data['name']))$this->name=$data['name'];
 if(isset($data['email']))$this->email=$data['email'];
 if(isset($data['username']))$this->username=$data['username'];
+if(isset($data['reseturl']))$this->reseturl=$data['reseturl'];
 if(isset($data['pubdate']))$this->pubdate=$data['pubdate']:
 if(isset($data['ip']))$this->ip=int($data['ip']);
 
@@ -36,29 +38,26 @@ $pubdate=mktime(0,0,0,$d,$m,$y);
 }
 public static function getUser($username,$password){
 $conn= new PDO(DB_DSN,DB_USERNAME,DB_PASSWORD);
-$sql="SELECT*, UNIX_TIMESTAMP(pubdate) AS pubdate where
- url=:url";
+$sql="SELECT* FROM user where (username=:username OR email=:username)AND password=:password";
 $stmt=$conn->prepare($sql);
-$stmt->bindValue(":url",$this->url, PDO::PARAM_STR);
-$stmt->execute;
+$stmt->bindValue(":username",$_POST['username'], PDO::PARAM_STR);
+$stmt->bindValue(":email",$_POST['username'], PDO::PARAM_STR);
+$stmt->bindValue(":password",$_POST['password'], PDO::PARAM_STR);
 $row = stmt->fetch();
 $conn = null;
-if($row) $Post = new Post($row);
+if($row) $user = new User($row);
 
 }
 public function insert(){
 $conn= new PDO(DB_DSN,DB_USERNAME,DB_PASSWORD);
-$sql = "INSERT INTO posts(title,description,summary,content,url,imageurl,image_extension,categoryID,pubdate,ip)
-VALUES(:title,:description,:summary,:content,:url,:imageurl,:image_extension,:categoryID,:pubdate,:ip)";
+$sql = "INSERT INTO user(name,email,username,reseturl,pubdate,ip)
+VALUES(:name,:email,:username,:reseturl,:pubdate,:ip)";
 $stmt=conn->prepare($sql);
 , PDO::PARAM_STR);
-$stmt->bindValue(":description",$th$stmt->bindValue(":title",$this->titleis->description, PDO::PARAM_STR);
-$stmt->bindValue(":summary",$this->summary, PDO::PARAM_STR);
-$stmt->bindValue(":content",$this->content, PDO::PARAM_STR);
-$stmt->bindValue(":url",$this->url, PDO::PARAM_STR);
-$stmt->bindValue(":imageurl",$this->imageurl, PDO::PARAM_STR);
-$stmt->bindValue(":image_extension",$this->image_extension, PDO::PARAM_STR);
-$stmt->bindValue(":categoryID",$this->categoryID, PDO::PARAM_STR);
+$stmt->bindValue(":name",$this->name,bindValue(":name" PDO::PARAM_STR);
+$stmt->bindValue(":email",$this->email, PDO::PARAM_STR);
+$stmt->bindValue(":username",$this->username, PDO::PARAM_STR);
+$stmt->bindValue(":reseturl",$this->reseturl, PDO::PARAM_STR);
 $stmt->bindValue(":pubdate",$this->pubdate, PDO::PARAM_STR);
 $stmt->bindValue(":ip",$this->ip, PDO::PARAM_STR);
 $stmt->execute;
