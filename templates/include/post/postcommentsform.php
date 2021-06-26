e2579ab8985511409adc7ae3977df03d0b4bf2d0
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-26 07:32:09 +0100

    Update postcommentsform.php

--- templates/include/post/postcommentsform.php
+++ templates/include/post/postcommentsform.php
@@ -3,5 +3,5 @@ Name:<input type="text" name="name" placeholder="Type In Your Name">
 Email:<input type="text" name="email" placeholder ="Type in your Email">
 Website:<input type="text" name="website" placeholder ="Type in your Website">
 Comments:<textarea  name="comments" placeholder =" typeset in your comments"></textare>
-<input type="submit" name="submit" value="savecomments" onload="loadComments()">
+<input type="submit" name="submit" value="savecomments" onclick="loadComments()">
 </form>
