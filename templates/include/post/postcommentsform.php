4cde6beb9dbc5c2f59e89260024f1fac07144607
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-26 07:10:22 +0100

    Create postcommentsform.php

---
+++ templates/include/post/postcommentsform.php
@@ -0,0 +1,8 @@
+﻿<form action="admin.php method="post">
+Username:<input type="text" name="username">
+Firstname:<input type="text" name="firstname" placeholder="Type In Your Firstname">
+Lastname:<input type="text" name="lastname" placeholder ="Type in your Lastname">
+Password:<input type="text" name="password" placeholder ="Type in your password">
+Confirm Password:<input type="text" name="password" placeholder ="Re type in your password">
+<input type="submit" name="submit" value="Register">
+</form>
