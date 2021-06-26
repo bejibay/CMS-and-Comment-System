51bd7c48a1a9503e9996e84d9e7b43b8af6d5811
bejibay <58639255+bejibay@users.noreply.github.com>
2021-06-26 11:02:42 +0100

    Create loadcomments.js

--- loadcomments.js
+++ loadcomments.js
@@ -1,14 +1,10 @@
 function loadComments(){
-
 var xhr=new XMLHttpRequest();
-xhr.onreadystatechange=function(){
-if(this.readyState==200 && this.status==4){
+xhr.onload=function(){
 document.getElementById("loadcomments").innerHTML=
-xhr.responseTxt;
+xhr.responseText;
 }
-}
-xhr.open("POST","/comments.php" "true");
-xhr.responseheader();
+xhr.open("GET","/comments.php" true);
 xhr.send();
-
 }
+
