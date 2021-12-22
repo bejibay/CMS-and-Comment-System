2b4010c998106d0cd754d1a06f26dcb4c458a0e1
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-22 15:39:14 +0100

    Update homepage.php

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,8 +1,8 @@
 <html>
 <head>
+<title><?php echo $results['title'];?></title>
 <meta  name="description" content="<?php echo $results['description'];?>">
 <meta  name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo $results['title'];?></title>
 </head>
 <body>
 <div class="header"><?php include "templates/include/header.php";?></div>
