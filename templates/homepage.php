0b4753e39a9be97e884a71482812be1491dcca76
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-22 15:46:14 +0100

    Update homepage.php

--- templates/homepage.php
+++ templates/homepage.php
@@ -11,5 +11,9 @@
  Put the homepage contents here</div>
 </div class="column rightside">put this column content here</div>
 </div>
+<div class="footet"><?php include "templates/include/footer.php";?></div>
+</body>
+</html>
+
 
 
