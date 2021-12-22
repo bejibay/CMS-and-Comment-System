8be458a537b25dda91d74c2ede8b93997843145f
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-22 16:34:10 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,24 +1,20 @@
 
-
-// config codes
-
 <?php 
-
 define("DB_DSN", "mysql:host=localhost;dbname=soowecco");
 define("DB_USERNAME", "soowecca");
 define("DB_PASSWORD", " ");
 define("CLASS_PATH", "classes");
-define("TEMPLATE_PATH", "templates"):
-require(classes."/article.php");
-require(classes."/category");
-require(classes."/admin.php");
-require(functions.php);
+define("TEMPLATE_PATH", "templates");
+require(CLASS_PATH."/page.php");
+require(CLASS_PATH."/post.php");
+require(CLASS_PATH."/category");
+require(CLASS_PATH."/user.php");
+require("functions.php");
 function handle_execption($exception){
 echo "there is an error, try again";
 error_log($exeption->getMessage);
 set_exception_handler(handle_exception);
 }
-
 ?>
 
 
