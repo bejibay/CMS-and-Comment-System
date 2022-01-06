645420de65bc5b254bbcb4ea8999a8027325ce07
bejibay <58639255+bejibay@users.noreply.github.com>
2022-01-06 07:07:57 +0100

    Create resetform.php

---
+++ templates/admin/resetform.php
@@ -0,0 +1,5 @@
+﻿<form action="admin.php" method="post">
+<label for="logininfo">Login Info:</label>
+<input type="text" name="logininfo" placeholder="Type in your username or email" id="logininfo">
+<input type="submit" name="reset" value="Reset">
+</form>
