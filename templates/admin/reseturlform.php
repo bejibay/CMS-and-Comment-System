f2d943888a9f1ccc43e029ea9afebe2c1fd68cca
bejibay <58639255+bejibay@users.noreply.github.com>
2022-01-06 07:03:36 +0100

    Update and rename resetform.php to reseturlform.php

---
+++ templates/admin/reseturlform.php
@@ -0,0 +1,9 @@
+﻿<form action="admin.php" method="post">
+<label for="logininfo">Login Info:</>
+<input type="text" name="logininfo" placeholder="Type in your username or email" id="logininfo">
+<label for="password">Password:</>
+<input type="text" name="password" placeholder ="Type in your new password">
+<label for="confirmpassworf">Confirm Password:</>
+<input type="text" name="confirmpassword" placeholder ="Re type in your new password">
+<input type="submit" name="reset" value="Reset">
+</form>
