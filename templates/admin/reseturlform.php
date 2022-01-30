4a7945a5a2cba384ae6459faa8476ffbc92e84ad
bejibay <58639255+bejibay@users.noreply.github.com>
2022-01-31 00:11:54 +0100

    Update reseturlform.php

--- templates/admin/reseturlform.php
+++ templates/admin/reseturlform.php
@@ -1,3 +1,14 @@
+
+<html>
+<head>
+<title><?php echo $results['title'];?></title>
+<meta  name="description" content="<?php echo $results['description'];?>">
+<meta  name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div class="header"><?php include "templates/include/header.php";?></div>
+<div class="row">
+<div class="column leftside">
 ﻿<form action="admin.php" method="post">
 <label for="logininfo">Login Info:</label>
 <input type="text" name="logininfo" placeholder="Type in your username or email" id="logininfo">
@@ -7,3 +18,9 @@
 <input type="text" name="confirmpassword" placeholder ="Re type in your new password">
 <input type="submit" name="reset" value="Reset">
 </form>
+</div>
+</div class="column rightside"></div>
+</div>
+<div class="footet"><?php include "templates/include/footer.php";?></div>
+</body>
+</html>
