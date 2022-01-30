9e33c3994da5586de8f259700ff75684b5e9e90a
bejibay <58639255+bejibay@users.noreply.github.com>
2022-01-31 00:21:03 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -2,28 +2,6 @@
 //Admin codes for login, register, and  password reset
 require("config.php");
 $action=isset($_GET['action'])? $_GET['action'] : "";
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta  name="description" content="<?php echo $result['description'];?>">
-<meta  name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="cmsstyle.css">
-<title><?php echo $result['title'];?></title>
-</head>
-<body>
-<div class="container">
-<div class="topnav">
-<div class="myLinks">
-<a href="/register">Register</a>
-<a href="/login">login</a>
-<a href="/reset">Reset Login</a>
-<a href="javascript:void();"><i  class ="fa fa-bars" ></i></a>
-</div>
-</div>
-<div class="row">
-<div class="col-12">
-<?php
 switch($action){
 case "login":
 login();
@@ -39,13 +17,7 @@ reset();
 break;
 }
 ?>
-</div>
-</div>
-<div class="footer"><?php include "templates/include/footer.php";?>
-</div>
-</div>
-</body>
-</html>
+
 
 
 
