f50b1952016bb0d3940394490f366f96741d3fb9
bejibay <58639255+bejibay@users.noreply.github.com>
2022-02-07 18:57:03 +0100

    Delete postcommentsform.php

--- templates/include/post/postcommentsform.php
+++
@@ -1,7 +0,0 @@
-﻿<form action="admin.php method="post">
-Name:<input type="text" name="name" placeholder="Type In Your Name">
-Email:<input type="text" name="email" placeholder ="Type in your Email">
-Website:<input type="text" name="website" placeholder ="Type in your Website">
-Comments:<textarea  name="comments" placeholder =" typeset in your comments"></textare>
-<input type="submit" name="submit" value="savecomments" onclick="loadComments()">
-</form>
