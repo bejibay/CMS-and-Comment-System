0acdd668a654ec2cacb8c6b79ab7862adb1ccff7
bejibay <58639255+bejibay@users.noreply.github.com>
2022-02-08 23:31:52 +0100

    Update loginform.php

--- templates/admin/loginform.php
+++ templates/admin/loginform.php
@@ -1,8 +1,10 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
 <title><?php echo $results['title'];?></title>
 <meta  name="description" content="<?php echo $results['description'];?>">
 <meta  name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="cmsstyle.css>"
 </head>
 <body>
 <div class="header"><?php include "templates/include/header.php";?></div>
