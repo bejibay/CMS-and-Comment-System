fc0b6dbcab03f5bf0ad541fbd0effc80e78822d1
bejibay <58639255+bejibay@users.noreply.github.com>
2022-03-16 15:30:41 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -134,6 +134,36 @@ require(TEMPLATE_PATH."/editpost.php");
 }
 }
 
+function createcategory(){
+$results= array();
+$results['title'] = "Create A New Category";
+$results['description'] = "Create A New Category";
+$results['formAction']="newCategory";
+$results['pageheading']="Create A Category";
+$createcategory =new Category;
+if(isset($_POST['createcategory'])){
+$createcategory->storeForm($_POST);
+$createcategory->insert();
+require(TEMPLATE_PATH."/editcategory.php");
+}
+}
+
+function editcategory(){
+$results= array();
+$results['title'] = "Edit A Category";
+$results['description'] = "Edit A Category";
+$results['formAction']="newCategory";
+$results['pageheading']="Edit This Category";
+$editcategory =new Category;
+if(isset($_POST['editcategory'])){
+$editcategory->storeForm($_POST);
+$editcategory->ipdate();
+require(TEMPLATE_PATH."/editcategory.php");
+}
+}
+
+
+
 function viewpages(){
 Page::getList();
 foreach($row as $row){
